afde2011ccff91b0fa0b8f036f4b507688584437
Lee Hour <lhour999@gmail.com>
2023-01-18 15:43:24 +0700

    registered routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
-import {} from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AdminDashbord from "./pages/AdminDashbord";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Upload from "./pages/Upload";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/:name",
+    element: <Profile />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/manuals",
+        element: <Home />,
+      },
+      {
+        path: "/upload-manual",
+        element: <Upload />,
+      },
+      {
+        path: "pending-manuals",
+        element: <AdminDashbord />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-cyan-800 ">asdfa</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/AdminDashbord.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminDashbord = () => {
+  return <div>AdminDashbord</div>;
+};
+
+export default AdminDashbord;
